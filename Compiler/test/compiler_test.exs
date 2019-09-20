c08f2389b2df019152237b6850097b8fbae8c861
EnriqueGuerreroL <chikehp710@yahoo.com>
2019-09-20 00:38:27 -0500

    [ADD] Valid tests

--- Compiler/test/compiler_test.exs
+++ Compiler/test/compiler_test.exs
@@ -3,8 +3,8 @@ defmodule COMPILERTest do
   doctest COMPILER
 
   setup_all do
-    {:ok, tokens: [:intKeyword,
-                  {:identifier, "int"},
+    {:ok, tok_ret2: [:intKeyword,
+                  {:identifier, "main"},
                   :openParen,
                   :closeParen,
                   :openBrace,
@@ -15,7 +15,53 @@ defmodule COMPILERTest do
     ]}
   end
 
+  setup_all do
+    {:ok, tok_ret0: [:intKeyword,
+                  {:identifier, "main"},
+                  :openParen,
+                  :closeParen,
+                  :openBrace,
+                  :returnKeyword,
+                  {:constant, 0},
+                  :semicolon,
+                  :closeBrace
+    ]}
+  end
+
+  setup_all do
+    {:ok, tok_multidig: [:intKeyword,
+                  {:identifier, "main"},
+                  :openParen,
+                  :closeParen,
+                  :openBrace,
+                  :returnKeyword,
+                  {:constant, 100},
+                  :semicolon,
+                  :closeBrace
+    ]}
+  end
+
   test "Return 2", state do
-    assert Lexer.lexer([test/nosaS_tests/valid/return_2.c]) == state[:tokens]
+    assert Lexer.lexer([test/noraS_tests/valid/return_2.c]) == state[:tok_ret2]
+  end
+
+  test "Return 0", state do
+    assert Lexer.lexer([test/noraS_tests/valid/return_0.c]) == state[:tok_ret0]
+  end
+
+  test "Multidigit", state do
+    assert Lexer.lexer([test/noraS_tests/valid/multi_digit.c]) == state[:tok_multidig]
+  end
+
+  test "Spaces", state do
+    assert Lexer.lexer([test/noraS_tests/valid/spaces.c]) == state[:tok_ret0]
+  end
+
+  test "No new lines", state do
+    assert Lexer.lexer([test/noraS_tests/valid/no_newlines.c]) == state[:tok_ret0]
+  end
+
+  test "New lines", state do
+    assert Lexer.lexer([test/noraS_tests/valid/newlines.c]) == state[:tok_ret0]
   end
 end
