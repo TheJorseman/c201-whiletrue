42791f3e2388c921b728d7fbaaacb539c96297e9
EnriqueGuerreroL <chikehp710@yahoo.com>
2019-10-14 23:18:59 -0500

    [ADD] Valid lexer tests

--- Compiler/test/compiler_test.exs
+++ Compiler/test/compiler_test.exs
@@ -24,17 +24,101 @@ defmodule COMPILERTest do
                 {:constant, 0, 2},
                 {:semicolon, "", 2},
                 {:closeBrace, "", 3}
+    ],
+
+    multi_digit: [{:intKeyword, "", 1},
+                {:identifier, "main", 1},
+                {:openParen, "", 1},
+                {:closeParen, "", 1},
+                {:openBrace, "", 1},
+                {:returnKeyword, "", 2},
+                {:constant, 100, 2},
+                {:semicolon, "", 2},
+                {:closeBrace, "", 3}
+    ],
+    no_newlines: [{:intKeyword, "", 1},
+                {:identifier, "main", 1},
+                {:openParen, "", 1},
+                {:closeParen, "", 1},
+                {:openBrace, "", 1},
+                {:returnKeyword, "", 1},
+                {:constant, 0, 1},
+                {:semicolon, "", 1},
+                {:closeBrace, "", 1}
+    ],
+    newlines: [{:intKeyword, "", 2},
+                {:identifier, "main", 3},
+                {:openParen, "", 4},
+                {:closeParen, "", 5},
+                {:openBrace, "", 6},
+                {:returnKeyword, "", 7},
+                {:constant, 0, 8},
+                {:semicolon, "", 9},
+                {:closeBrace, "", 10}
     ]
     }
 
   end
 
-  # Valid lexer tests
-  test "Return 2", state do
-    assert Compiler.print_token_list("test/noraS_tests/valid/return_2.c") == state[:tok_ret2]
+  # Valid lexer tests -----------------------------------
+
+  test "1. Return 2", state do
+    source_code = """
+                  int main() {
+                    return 2;
+                  }
+                  """
+    assert Lexer.lexer(source_code) == state[:tok_ret2]
   end
 
-  test "Return 0", state do
-    assert Compiler.print_token_list("test/noraS_tests/valid/return_0.c") == state[:tok_ret0]
+  test "2. Return 0", state do
+    source_code = """
+                  int main() {
+                    return 0;
+                  }
+                  """
+    assert Lexer.lexer(source_code) == state[:tok_ret0]
   end
+
+  test "3. Multi digit", state do
+    source_code = """
+                  int main() {
+                    return 100;
+                  }
+                  """
+    assert Lexer.lexer(source_code) == state[:multi_digit]
+  end
+
+  test "4. No newlines", state do
+    source_code = """
+                  int main(){return 0;}
+                  """
+    assert Lexer.lexer(source_code) == state[:no_newlines]
+  end
+
+  # test "5. Newlines", state do
+  #   source_code = """
+
+  #                 int
+  #                 main
+  #                 (
+  #                 )
+  #                 {
+  #                 return
+  #                 0
+  #                 ;
+  #                 }
+  #                 """
+  #   assert Lexer.lexer(source_code) == state[:newlines]
+  # end
+
+  test "6. Spaces", state do
+    source_code = """
+                    int   main    (  )  {   return  0 ; }
+                  """
+    assert Lexer.lexer(source_code) == state[:no_newlines]
+  end
+
+
+
 end
