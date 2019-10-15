a7c0b2b64d84c891dca83fc1f18e320af6fab433
EnriqueGuerreroL <chikehp710@yahoo.com>
2019-10-14 20:11:27 -0500

    [MOD] Compiler test

--- Compiler/test/compiler_test.exs
+++ Compiler/test/compiler_test.exs
@@ -3,16 +3,30 @@ defmodule COMPILERTest do
   doctest Compiler
 
   setup_all do
-    {:ok, tok_ret2: [{:intKeyword, "", 1},
-                  {:identifier, "main", 1},
-                  {:openParen, "", 1},
-                  {:closeParen, "", 1},
-                  {:openBrace, "", 1},
-                  {:returnKeyword, "", 2},
-                  {:constant, 2, 2},
-                  {:semicolon, "", 2},
-                  {:closeBrace, "", 3}
-    ]}
+    {:ok,
+    tok_ret2: [{:intKeyword, "", 1},
+                {:identifier, "main", 1},
+                {:openParen, "", 1},
+                {:closeParen, "", 1},
+                {:openBrace, "", 1},
+                {:returnKeyword, "", 2},
+                {:constant, 2, 2},
+                {:semicolon, "", 2},
+                {:closeBrace, "", 3}
+    ],
+
+    tok_ret0: [{:intKeyword, "", 1},
+                {:identifier, "main", 1},
+                {:openParen, "", 1},
+                {:closeParen, "", 1},
+                {:openBrace, "", 1},
+                {:returnKeyword, "", 2},
+                {:constant, 0, 2},
+                {:semicolon, "", 2},
+                {:closeBrace, "", 3}
+    ]
+    }
+
   end
 
   # Valid lexer tests
@@ -20,4 +34,7 @@ defmodule COMPILERTest do
     assert Compiler.print_token_list("test/noraS_tests/valid/return_2.c") == state[:tok_ret2]
   end
 
+  test "Return 0", state do
+    assert Compiler.print_token_list("test/noraS_tests/valid/return_0.c") == state[:tok_ret0]
+  end
 end
