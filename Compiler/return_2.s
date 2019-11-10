0de374953f5eec12ea1dc29a865f8744ebe53d54
TheJorseman <miguel17546mals@gmail.com>
2019-11-09 18:32:35 -0600

    [ADD] Week 3

--- Compiler/return_2.s
+++ Compiler/return_2.s
@@ -1,9 +1,36 @@
 
-
         .globl main
             main:
-                movl $2, %eax
+                                                                                mov $12, %rax
+                push %rax
+                mov $3, %rax
+                pop %rbx
+                sub %rax, %rbx
+                mov %rbx, %rax
+
+                push %rax
+                mov $2, %rax
+                pop %rbx
+                imul %rbx, %rax
+
+                push %rax
+                mov $2, %rax
+                pop %rbx
+                add %rbx, %rax
+
+                push %rax
+                                mov $5, %rax
+                push %rax
+                mov $3, %rax
+                pop %rbx
+                sub %rax, %rbx
+                mov %rbx, %rax
+
+                pop %rbx
+                xchg %rax, %rbx
+                cqo
+                idiv %rbx
+
             ret
          
-
         
