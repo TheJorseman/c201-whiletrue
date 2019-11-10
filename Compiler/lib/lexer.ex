756bd639fb3215ebae9b75127471806e613a95bf
TheJorseman <miguel17546mals@gmail.com>
2019-11-10 02:30:38 -0600

    [FIX] error, change == before =

--- Compiler/lib/lexer.ex
+++ Compiler/lib/lexer.ex
@@ -74,14 +74,14 @@ defmodule Lexer do
         tokensRemaining(data,Token.orT(number_line),~r/^\|\|/)
       String.match?(sentence,~r/^==/) ->
         tokensRemaining(data,Token.equal(number_line),~r/^==/)
-      String.match?(sentence,~r/^</) ->
-        tokensRemaining(data,Token.lessThan(number_line),~r/^</)
       String.match?(sentence,~r/^<=/) ->
         tokensRemaining(data,Token.lessThanEq(number_line),~r/^<=/)
-      String.match?(sentence,~r/^>/) ->
-        tokensRemaining(data,Token.greaterThan(number_line),~r/^>/)
+      String.match?(sentence,~r/^</) ->
+        tokensRemaining(data,Token.lessThan(number_line),~r/^</)
       String.match?(sentence,~r/^>=/) ->
         tokensRemaining(data,Token.greaterThanEq(number_line),~r/^>=/)
+      String.match?(sentence,~r/^>/) ->
+        tokensRemaining(data,Token.greaterThan(number_line),~r/^>/)
       sentence == "" -> []
       sentence == " "-> []
       true ->
